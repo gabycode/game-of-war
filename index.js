b0c8b2ed6dd090cd8b608fe7245f8dc658de66e9
gabycode <86427446+gabycode@users.noreply.github.com>
2022-08-23 00:25:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const newDeckBtn = document.getElementById("new-deck-btn")
+const drawCardsBtn = document.getElementById("draw-cards")
 
 newDeckBtn.addEventListener("click", () => {
     console.log("button click")
+})
+
+drawCardsBtn.addEventListener("click", () => {
+    console.log("draw cards")
 })
