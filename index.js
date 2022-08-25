d417ba619b61ac58a5f423a4f6a970d27d646402
gabycode <86427446+gabycode@users.noreply.github.com>
2022-08-25 14:32:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
+let deckId
 const newDeckBtn = document.getElementById("new-deck-btn")
 const drawCardsBtn = document.getElementById("draw-cards")
+const remainingText = document.getElementById("remaining")
+const cardsContainer = document.getElementById("cards")
 
-newDeckBtn.addEventListener("click", () => {
-    console.log("button click")
-})
+newDeckBtn.addEventListener("click", handleClick)
+
+function handleClick() {
+    fetch("https://www.deckofcardsapi.com/api/deck/new/")
+        .then(res => res.json())
+        .then(data => {
+            remainingText.textContent = `Remaining Cards: ${data.remaining}`
+            deckId = data.deck_id
+            console.log(deckId)
+        })
+}
 
 drawCardsBtn.addEventListener("click", () => {
-    console.log("draw cards")
+    fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            remainingText.textContent = `Remaining Cards: ${data.remaining}`
+
+            cardsContainer.children[0].innerHTML = `<img src=${data.cards[0].image} class="card">`
+            cardsContainer.children[1].innerHTML = `<img src=${data.cards[1].image} class="card">`
+
+            if(drawCardsBtn === 0) {
+                drawCardsBtn.disabled = true
+            }
+        })
 })
