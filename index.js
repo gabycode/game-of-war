ab3794be6d522d30e5e0d8daa29dad31e53cff0a
gabycode <86427446+gabycode@users.noreply.github.com>
2022-09-01 12:55:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 let deckId
+let computerScore = 0
+let myScore = 0
 const newDeckBtn = document.getElementById("new-deck-btn")
 const drawCardsBtn = document.getElementById("draw-cards")
 const remainingText = document.getElementById("remaining")
 const cardsContainer = document.getElementById("cards")
-const myScore = document.getElementById("my-score")
-const computerScore = document.getElementById("computer-score")
+const myScoreEl = document.getElementById("my-score")
+const computerScoreEl = document.getElementById("computer-score")
 const header = document.getElementById("header")
+const cards = document.getElementById("cards")
+const instr = document.getElementById("instr")
+
 
 newDeckBtn.addEventListener("click", handleClick)
 
 function handleClick() {
-    fetch("https://www.deckofcardsapi.com/api/deck/new/")
+    fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
             remainingText.textContent = `Remaining Cards: ${data.remaining}`
@@ -28,24 +33,44 @@ drawCardsBtn.addEventListener("click", () => {
             cardsContainer.children[0].innerHTML = `<img src=${data.cards[0].image} class="card">`
             cardsContainer.children[1].innerHTML = `<img src=${data.cards[1].image} class="card">`
 
-            // const winnerText = determineCardWinner(data.cards[0], data.cards[1])
-            // header.textContent = winnerText
+            const winnerText = determineCardWinner(data.cards[0], data.cards[1])
+            header.textContent = winnerText
             
             if(data.remaining === 0) {
-                drawCardsBtn.disabled = true
-                console.log("no more cards")
+                    drawCardsBtn.disabled = true
+                    console.log("no more cards")
+                if(computerScore > myScore) {
+                    header.textContent = `Computer is the final winner`
+                    instr.textContent = ` `
+                    cards.textContent = ` `
+                } else if (computerScore < myScore) {
+                    header.textContent = `You are the final winner`
+                    instr.textContent = ` `
+                    cards.textContent = ` `
+                } else {
+                    header.textContent = `It's a tie!`
+                    instr.textContent = ` `
+                    cards.textContent = ` `
+                }
             }
         })
 })
 
-// function determineCardWinner(card1, card2) {
-//     const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
-//     "10", "JACK", "QUEEN", "KING", "ACE"]
-//     const card1ValueIndex = valueOptions.indexOf(card1.value)
-//     const card2ValueIndex = valueOptions.indexOf(card2.value)
-//     // console.log(card1ValueIndex)
-//     // console.log(card2ValueIndex)
-//     // if(card1ValueIndex > card2ValueIndex) {
-        
-//     // }
-// }
+function determineCardWinner(card1, card2) {
+    const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
+    "10", "JACK", "QUEEN", "KING", "ACE"]
+    const card1ValueIndex = valueOptions.indexOf(card1.value)
+    const card2ValueIndex = valueOptions.indexOf(card2.value)
+    
+    if(card1ValueIndex > card2ValueIndex) {
+        computerScore++
+        computerScoreEl.textContent = `Computer Score: ${computerScore}`
+        return "Computer wins!"
+    } else if(card1ValueIndex < card2ValueIndex) {
+        myScore++
+        myScoreEl.textContent = `My Score: ${myScore}`
+        return "You win!"
+    } else {
+        return "War!"
+    }
+}
